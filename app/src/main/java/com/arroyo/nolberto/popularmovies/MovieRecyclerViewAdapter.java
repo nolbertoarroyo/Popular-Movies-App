a4b44f18332391d89c7a638e06975e3c1f4a7b9f
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2018-05-10 09:27:04 -0700

    generated movie model for api call

--- app/src/main/java/com/arroyo/nolberto/popularmovies/MovieRecyclerViewAdapter.java
+++ app/src/main/java/com/arroyo/nolberto/popularmovies/MovieRecyclerViewAdapter.java
@@ -1,29 +1,57 @@
 package com.arroyo.nolberto.popularmovies;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MovieRecyclerViewAdapter extends RecyclerView.Adapter{
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+import com.arroyo.nolberto.popularmovies.Model.MovieModel;
+
+import java.util.ArrayList;
+
+public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieViewHolder>{
+    ArrayList<MovieModel> moviesList;
+
+    public MovieRecyclerViewAdapter(ArrayList<MovieModel> moviesList) {
+        this.moviesList = moviesList;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        int listItem= R.layout.list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(listItem,parent,false);
+        MovieViewHolder viewHolder = new MovieViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return moviesList.size();
     }
 
-    public class MovieViewHolder extends RecyclerView.ViewHolder{
 
-        public MovieViewHolder(View itemView) {
-            super(itemView);
-        }
+}
+class MovieViewHolder extends RecyclerView.ViewHolder{
+
+    ImageView movieThumbnail;
+    TextView movieTitle;
+
+    public MovieViewHolder(View itemView) {
+        super(itemView);
+
+        this.movieThumbnail = (ImageView)itemView.findViewById(R.id.movie_thumbnail_iv);
+        this.movieTitle = (TextView)itemView.findViewById(R.id.movie_title_tv);
     }
 }
