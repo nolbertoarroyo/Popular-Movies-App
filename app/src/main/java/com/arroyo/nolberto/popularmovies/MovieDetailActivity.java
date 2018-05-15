b0b3917b3e15689d67901c818bdb7e0067ff646f
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2018-05-14 21:46:59 -0700

    added back navigation for detailActivity

--- app/src/main/java/com/arroyo/nolberto/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/arroyo/nolberto/popularmovies/MovieDetailActivity.java
@@ -1,6 +1,7 @@
 package com.arroyo.nolberto.popularmovies;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -18,9 +19,15 @@ public class MovieDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
 
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+
         moviePoster = (ImageView)findViewById(R.id.detail_movie_thumbnail_iv);
         overViewTv = (TextView)findViewById(R.id.movie_description_tv);
         movieTitleTv = (TextView)findViewById(R.id.detail_movie_title_tv);
+        movieReleaseDateTv = (TextView)findViewById(R.id.movie_release_date_tv);
 
         Intent receivedIntent = getIntent();
         Response.MoviesModel movie= receivedIntent.getParcelableExtra("MovieSelected");
@@ -28,6 +35,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         overViewTv.setText(movie.getOverview());
         movieTitleTv.setText(movie.getOriginal_title());
         String pic = getApplicationContext().getString(R.string.movie_image_base_url)+ movie.getPoster_path();
+        movieReleaseDateTv.setText(movie.getRelease_date());
 
         Picasso.with(this).load(pic).into(moviePoster);
 
